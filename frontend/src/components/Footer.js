0ace2c96f252342d6c12f4d657e3fac08fc5f4ff
Naveen T <naveenthiyagu42@gmail.com>
2025-05-30 08:29:44 +0530

    Ecom Website

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return  <footer className="py-1 bg-dark">
+                <p className="text-center text-white mt-1 ">
+                    Ecom Shop 2025, All Rights Reserved
+                </p>
+            </footer>
+}
